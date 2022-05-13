5e321e8719876fd37b431c3fccebe9098c1cc54a
Juan Pablo <Juan Pablo@LAPTOP-SOCJD2T1>
2022-05-13 15:59:46 -0500

    por

--- src/main/java/com/example/portada/Controllers/DemoController.java
+++ src/main/java/com/example/portada/Controllers/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-	@GetMapping("/")
+	@GetMapping("/demo/portada")
 	public String Portada()
 	{
 		return "Portada";
